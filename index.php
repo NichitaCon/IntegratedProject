6052d57c3acf72c60ea5eca770a561c77de14e99
Nichita Condrea (Student) <N00233189@iadt.ie>
2024-04-15 19:53:34 +0100

    section 3 finished

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once "./story.php";
 
 $mainStorie = Story::findAll($options = array('limit' => 1, 'offset' => 9));
 
-$travelBlock = Story::findByCategory(3, $options = array('limit' => 9, 'offset' => 0));
+
 
 $photoBlock = Story::findByCategory(1, $options = array('limit' => 5, 'offset' => 0));
 
@@ -16,7 +16,23 @@ $photoBlockS3 = $photoBlock[2];
 $photoBlockS4 = $photoBlock[3];
 $photoBlockS5 = $photoBlock[4];
 
-echo $photoBlockS2 -> img_url;
+$techBlock = Story::findByCategory(2, $options = array('limit' => 5, 'offset' => 0));
+
+$techBlockS1 = $techBlock[0];
+$techBlockS2 = $techBlock[1];
+$techBlockS3 = $techBlock[2];
+$techBlockS4 = $techBlock[3];
+$techBlockS5 = $techBlock[4];
+
+$socialBlock = Story::findByCategory(3, $options = array('limit' => 5, 'offset' => 0));
+
+$socialBlockS1 = $socialBlock[0];
+$socialBlockS2 = $socialBlock[1];
+$socialBlockS3 = $socialBlock[2];
+$socialBlockS4 = $socialBlock[3];
+$socialBlockS5 = $socialBlock[4];
+
+echo $socialBlockS1 -> headline;
 
 
 $authorId = 7;
@@ -130,7 +146,7 @@ $mainStory = Story::findById($myId);
 
 
 
-    <!-- Photography session -->
+    <!-- Photography section -->
     <section class="Photography">
         <div class="container">
             <h2 class="sub-title width-12">
@@ -139,50 +155,55 @@ $mainStory = Story::findById($myId);
 
             <!-- Tall component left side 1-->
             <?php  { ?>
-            <div class="width-3 portrude tallComponent">
+            <div class="width-3 portrude tallComponent ver1">
                 <style>
-                    .tallComponent {
+                    .Photography .tallComponent{
+                        
                         background-image: url("images/<?=$photoBlockS1 -> img_url ?>");
+                        
                     }
                 </style>
                 <div class="whiteBox">
-                    <h2><?= substr($photoBlockS1->headline, 0 , 52) ?></h2>
+                    <h2><?= substr($photoBlockS1->headline, 0 , 63) ?></h2>
                     <div class="category">
                         <p class="author"><?= Author::findById($photoBlockS1->author_id)->first_name . " " . Author::findById($photoBlockS1->author_id)->last_name ?></p>
-                        <p class="date">3 HOURS AGO</p>
+                        <p class="date"><?= substr($photoBlockS1->created_at, 0 , 52) ?></p>
                     </div>
                 </div>
-                
+                <?php } ?>
             </div>
 
             <!-- Top section -->
             <div class="width-9 combined">
                 <div class="width-9 holder top">
+                
                     <div class="width-3 portrude mediumComponent">
                         <style>
-                            .mediumComponent {
+                            .Photography .mediumComponent {
                                 background-image: url("images/<?=$photoBlockS2 -> img_url ?>");
                             }
                         </style>
                         <div class="whiteBox">
-                            <h2>Strong US growth boosts expectation that Fed will delay cutting rates</h2>
+                            <h2><?= substr($photoBlockS2->headline, 0 , 63) ?></h2>
                             <div class="category">
-                                <p class="author">US interest rates</p>
-                                <p class="date">3 HOURS AGO</p>
+                                <p class="author"><?= Author::findById($photoBlockS2->author_id)->first_name . " " . Author::findById($photoBlockS2->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($photoBlockS2->created_at, 0 , 52) ?></p>
                             </div>
                         </div>
+                       
                     </div>
-                    <div class="width-6 portrude longComponent">
+
+                    <div class="width-6 portrude longComponent ver1">
                         <style>
-                            .longComponent {
+                            .Photography .longComponent.ver1 {
                                 background-image: url("images/<?=$photoBlockS3 -> img_url ?>");
                             }
                         </style>
                         <div class="whiteBox">
-                            <h2>Strong US growth boosts expectation that Fed will delay cutting rates</h2>
+                            <h2><?= substr($photoBlockS3->headline, 0 , 63) ?></h2>
                             <div class="category">
-                                <p class="author">US interest rates</p>
-                                <p class="date">3 HOURS AGO</p>
+                                <p class="author"><?= Author::findById($photoBlockS3->author_id)->first_name . " " . Author::findById($photoBlockS3->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($photoBlockS3->created_at, 0 , 52) ?></p>
                             </div>
                         </div>
                     </div>
@@ -190,28 +211,244 @@ $mainStory = Story::findById($myId);
 
                 <!-- bottom section -->
                 <div class="width-9 holder">
-                    <div class="width-6 portrude longComponent">
+                <div class="width-6 portrude longComponent ver2">
+                        <style>
+                            .Photography .longComponent.ver2{
+                                background-image: url("images/<?=$photoBlockS4 -> img_url ?>");
+                            }
+                        </style>
                         <div class="whiteBox">
-                            <h2>Strong US growth boosts expectation that Fed will delay cutting rates</h2>
+                            <h2><?= substr($photoBlockS4->headline, 0 , 63) ?></h2>
                             <div class="category">
-                                <p class="author">US interest rates</p>
-                                <p class="date">3 HOURS AGO</p>
+                                <p class="author"><?= Author::findById($photoBlockS4->author_id)->first_name . " " . Author::findById($photoBlockS4->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($photoBlockS4->created_at, 0 , 52) ?></p>
                             </div>
                         </div>
                     </div>
                     
                     <!-- cardComponent -->
                     <div class="width-3 portrude cardComponent">
+                        <style>
+                            .Photography .cardComponent {
+                                background-image: url("images/<?=$photoBlockS5 -> img_url ?>");
+                            }
+                        </style>
                         <div class="whiteBox">
-                            <h2>Strong US growth boosts expectation that Fed will delay cutting rates</h2>
+                            <h2><?= substr($photoBlockS5->headline, 0 , 63) ?></h2>
                             <div class="category">
-                                <p class="author">US interest rates</p>
-                                <p class="date">3 HOURS AGO</p>
+                                <p class="author"><?= Author::findById($photoBlockS5->author_id)->first_name . " " . Author::findById($photoBlockS5->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($photoBlockS5->created_at, 0 , 52) ?></p>
                             </div>
                         </div>
                     </div>
                 </div>
+                
+            </div>
+
+
+        </div>
+    </section>
+
+        <!-- technology section -->
+    <section class="technology">
+        <div class="container">
+            <h2 class="sub-title width-12">
+                Technology
+            </h2>
+
+            <!-- Top section -->
+            <div class="width-9 combined">
+                <div class="width-9 holder top">
+                
+                    <div class="width-3 portrude mediumComponent">
+                        <style>
+                            .technology .mediumComponent {
+                                background-image: url("images/<?=$techBlockS2 -> img_url ?>");
+                            }
+                        </style>
+                        <div class="whiteBox">
+                            <h2><?= substr($techBlockS2->headline, 0 , 63) ?></h2>
+                            <div class="category">
+                                <p class="author"><?= Author::findById($techBlockS2->author_id)->first_name . " " . Author::findById($techBlockS2->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($techBlockS2->created_at, 0 , 52) ?></p>
+                            </div>
+                        </div>
+                       
+                    </div>
+
+                    <div class="width-6 portrude longComponent ver1">
+                        <style>
+                            .technology .longComponent.ver1 {
+                                background-image: url("images/<?=$techBlockS3 -> img_url ?>");
+                            }
+                        </style>
+                        <div class="whiteBox">
+                            <h2><?= substr($techBlockS3->headline, 0 , 63) ?></h2>
+                            <div class="category">
+                                <p class="author"><?= Author::findById($techBlockS3->author_id)->first_name . " " . Author::findById($techBlockS3->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($techBlockS3->created_at, 0 , 52) ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- bottom section -->
+                <div class="width-9 holder">
+                <div class="width-6 portrude longComponent ver2">
+                        <style>
+                            .technology .longComponent.ver2{
+                                background-image: url("images/<?=$techBlockS4 -> img_url ?>");
+                            }
+                        </style>
+                        <div class="whiteBox">
+                            <h2><?= substr($techBlockS4->headline, 0 , 63) ?></h2>
+                            <div class="category">
+                                <p class="author"><?= Author::findById($techBlockS4->author_id)->first_name . " " . Author::findById($techBlockS4->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($techBlockS4->created_at, 0 , 52) ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- cardComponent -->
+                    <div class="width-3 portrude cardComponent">
+                        <style>
+                            .technology .cardComponent {
+                                background-image: url("images/<?=$techBlockS1 -> img_url ?>");
+                            }
+                        </style>
+                        <div class="whiteBox">
+                            <h2><?= substr($techBlockS1->headline, 0 , 63) ?></h2>
+                            <div class="category">
+                                <p class="author"><?= Author::findById($techBlockS1->author_id)->first_name . " " . Author::findById($techBlockS1->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($techBlockS1->created_at, 0 , 52) ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+            <!-- Tall component right side -->
+            <?php  { ?>
+            <div class="width-3 portrude tallComponent ver1">
+                <style>
+                    .technology .tallComponent{
+                        
+                        background-image: url("images/<?=$techBlockS5 -> img_url ?>");
+                        
+                    }
+                </style>
+                <div class="whiteBox">
+                    <h2><?= substr($techBlockS5->headline, 0 , 63) ?></h2>
+                    <div class="category">
+                        <p class="author"><?= Author::findById($techBlockS5->author_id)->first_name . " " . Author::findById($techBlockS5->author_id)->last_name ?></p>
+                        <p class="date"><?= substr($techBlockS5->created_at, 0 , 52) ?></p>
+                    </div>
+                </div>
                 <?php } ?>
+            </div>
+
+
+        </div>
+    </section>
+
+    <!-- social section -->
+    <section class="social">
+        <div class="container">
+            <h2 class="sub-title width-12">
+                Social media
+            </h2>
+
+            <!-- Tall component left side 1-->
+            <?php  { ?>
+            <div class="width-3 portrude tallComponent ver1">
+                <style>
+                    .social .tallComponent{
+                        
+                        background-image: url("images/<?=$socialBlockS1 -> img_url ?>");
+                        
+                    }
+                </style>
+                <div class="whiteBox">
+                    <h2><?= substr($socialBlockS1->headline, 0 , 62) ?></h2>
+                    <div class="category">
+                        <p class="author"><?= Author::findById($socialBlockS1->author_id)->first_name . " " . Author::findById($socialBlockS1->author_id)->last_name ?></p>
+                        <p class="date"><?= substr($socialBlockS1->created_at, 0 , 52) ?></p>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+
+            <!-- Top section -->
+            <div class="width-9 combined">
+                <div class="width-9 holder top">
+                
+                    <div class="width-3 portrude mediumComponent">
+                        <style>
+                            .social .mediumComponent {
+                                background-image: url("images/<?=$socialBlockS2 -> img_url ?>");
+                            }
+                        </style>
+                        <div class="whiteBox">
+                            <h2><?= substr($socialBlockS2->headline, 0 , 63) ?></h2>
+                            <div class="category">
+                                <p class="author"><?= Author::findById($socialBlockS2->author_id)->first_name . " " . Author::findById($socialBlockS2->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($socialBlockS2->created_at, 0 , 52) ?></p>
+                            </div>
+                        </div>
+                       
+                    </div>
+
+                    <div class="width-6 portrude longComponent ver1">
+                        <style>
+                            .social .longComponent.ver1 {
+                                background-image: url("images/<?=$socialBlockS3 -> img_url ?>");
+                            }
+                        </style>
+                        <div class="whiteBox">
+                            <h2><?= substr($socialBlockS3->headline, 0 , 63) ?></h2>
+                            <div class="category">
+                                <p class="author"><?= Author::findById($socialBlockS3->author_id)->first_name . " " . Author::findById($socialBlockS3->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($socialBlockS3->created_at, 0 , 52) ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- bottom section -->
+                <div class="width-9 holder">
+                <div class="width-6 portrude longComponent ver2">
+                        <style>
+                            .social .longComponent.ver2{
+                                background-image: url("images/<?=$socialBlockS4 -> img_url ?>");
+                            }
+                        </style>
+                        <div class="whiteBox">
+                            <h2><?= substr($socialBlockS4->headline, 0 , 63) ?></h2>
+                            <div class="category">
+                                <p class="author"><?= Author::findById($socialBlockS4->author_id)->first_name . " " . Author::findById($socialBlockS4->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($socialBlockS4->created_at, 0 , 52) ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- cardComponent -->
+                    <div class="width-3 portrude cardComponent">
+                        <style>
+                            .social .cardComponent {
+                                background-image: url("images/<?=$socialBlockS5 -> img_url ?>");
+                            }
+                        </style>
+                        <div class="whiteBox">
+                            <h2><?= substr($socialBlockS5->headline, 0 , 70) ?></h2>
+                            <div class="category">
+                                <p class="author"><?= Author::findById($socialBlockS5->author_id)->first_name . " " . Author::findById($socialBlockS5->author_id)->last_name ?></p>
+                                <p class="date"><?= substr($socialBlockS5->created_at, 0 , 52) ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
 
 
